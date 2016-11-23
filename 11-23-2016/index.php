fa6f35034f26edf5257446a7674e54d5d7e4e004
Shahidul Islam <shawon_math07@yahoo.com>
2016-11-24 05:21:00 +0600

    Tutorial for CSS

---
+++ 11-23-2016/index.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<title>2nd class 11/23/16</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- Internal CSS -->
+		<style type="text/css">
+			h8{
+				color: blue;
+				font-size: 25px;
+			}
+			p{
+				color: gray;
+				font: "Times New Roman";
+				font-size: 15px;
+			}
+			/*Inline CSS > Internal CSS > External CSS 
+			h7{
+				color: blue;
+				font-size: 60px;
+			}*/
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<!-- Inline CSS  -->
+		<!-- Custom Tag h7,h8 -->
+		<h7 style="color:red;font-size:40px;">MakerLab</h7><br>
+		<hr>
+		<h8>Paragraph1</h8>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat.</p>
+		<hr>
+		<!-- External CSS -->
+		<h9>Paragraph2</h9><br>
+		<p1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p1>
+		<hr>
+		<h1 class="headline">Makerlab</h1>
+		<h1 class="headline" id="theader1">Makerlab</h1>
+		<h1 class="headline">Makerlab</h1>
+		<h1 class="headline">Makerlab</h1>
+		<h1>Makerlab</h1>
+		<h1>Makerlab</h1>
+		<hr>
+
+
+	</body>
+</html>
