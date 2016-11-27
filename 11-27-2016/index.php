b20079c4ea244729330c13faedd6ee4657e94dff
Shahidul Islam <shawon_math07@yahoo.com>
2016-11-27 11:52:52 +0600

    initial commit css 2nd class

---
+++ 11-27-2016/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>3rd class 11/27/16</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.</p>
+	</div>
+</body>
+</html>
