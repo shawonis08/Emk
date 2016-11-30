4b06059fd7e8608a4972025102ea424b53fbf497
Shahidul Islam <shawon_math07@yahoo.com>
2016-12-01 01:50:33 +0600

    how to mokup with html & css before website

---
+++ 11-30-2016/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>4th class CSS</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+	<!-- main page -->
+		<div class="main_container">
+		<!-- main page header -->
+			<div class="header">
+				
+			</div>
+			<!-- navigation bar -->
+			<div class="navbar">
+				
+			</div>
+			<!-- main content -->
+			<div class="wrap_container">
+				<!-- article -->
+				<div class="content">
+					
+				</div>
+				<!-- right navigation bar -->
+				<div class="right_navbar">
+					
+				</div>
+			</div>
+			<!-- main page footer -->
+			<div class="footer">
+					
+			</div>
+		</div>
+	</body>
+</html>
